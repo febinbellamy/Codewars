14689ea62ce37806cfa3fe918cf5f20ddfa023ed
Febin Bellamy <febinbellamy@gmail.com>
2022-08-30 18:33:09 -0400

    coding challenge

---
+++ GetTheMeanOfAnArray.js
@@ -0,0 +1,20 @@
+/* 
+8 kyu 
+
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+*/
+
+function getAverage(marks) {
+  //TODO : calculate the downward rounded average of the marks array
+  let sum = marks.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+  });
+
+  let numOfScores = marks.length;
+
+  return Math.floor(sum / numOfScores);
+}
