ec817a7dd79f69904aa9fd0be539c326560b7e5e
Febin Bellamy <febinbellamy@gmail.com>
2022-09-10 18:55:46 -0400

    codewars

---
+++ FindTheUniqueNumber.js
@@ -0,0 +1,27 @@
+/* 
+
+6 kyu
+
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+*/
+
+function findUniq(arr) {
+  // do magic
+  for (let i = 0; i < arr.length; i++) {
+    let currentElem = arr[i];
+    if (
+      currentElem !== arr[i + 1] &&
+      currentElem !== arr[i - 1] &&
+      currentElem !== arr[i + 2]
+    ) {
+      return currentElem;
+    }
+  }
+}
