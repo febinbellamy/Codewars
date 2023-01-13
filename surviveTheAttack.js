a4527d9fd7bb3da56895a5c481e73e185b753c0c
Febin Bellamy <febinbellamy@gmail.com>
2023-01-13 06:44:20 -0500

    Survive the attack

---
+++ surviveTheAttack.js
@@ -0,0 +1,60 @@
+/* 
+7 kyu
+
+Given two Arrays in which values are the power of each soldier, return true if you survive the attack or false if you perish.
+
+CONDITIONS
+- Each soldier attacks the opposing soldier in the same index of the array. The survivor is the number with the highest value.
+- If the value is the same they both perish
+- If one of the values is empty(different array lengths) the non-empty value soldier survives.
+- To survive the defending side must have more survivors than the attacking side.
+- In case there are the same number of survivors in both sides, the winner is the team with the highest initial attack power. If the total attack power of both sides is the same return true.
+- The initial attack power is the sum of all the values in each array.
+
+EXAMPLES
+attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4, 6, 8 ]  
+//0 survivors                4 survivors
+//return true
+
+attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4 ]  
+//2 survivors  (16 damage)   2 survivors (6 damage)
+//return false
+
+attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4, 0, 8 ]  
+//1 survivors                3 survivors 
+//return true
+*/
+
+function hasSurvived(attackers, defenders) {
+  let attackersSurvivors = 0;
+  let defendersSurvivors = 0;
+
+  for (let i = 0; i < attackers.length; i++) {
+    let currentAttacker = attackers[i];
+    let currentDefender = defenders[i];
+
+    if (currentAttacker > currentDefender) {
+      attackersSurvivors++;
+    } else if (currentAttacker < currentDefender) {
+      defendersSurvivors++;
+    }
+
+    if (typeof currentAttacker === "undefined") {
+      defendersSurvivors++;
+    } else if (typeof currentDefender === "undefined") {
+      attackersSurvivors++;
+    }
+  }
+
+  if (defendersSurvivors > attackersSurvivors) return true;
+  if (defendersSurvivors < attackersSurvivors) return false;
+  if (defendersSurvivors === attackersSurvivors) {
+    let attackersAttackPower = attackers.reduce((a, c) => a + c, 0);
+    let defendersAttackPower = defenders.reduce((a, c) => a + c, 0);
+    if (attackersAttackPower <= defendersAttackPower) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
