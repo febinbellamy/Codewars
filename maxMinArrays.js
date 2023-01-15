adf820215b88549b74e82e245c2dc9d244517143
Febin Bellamy <febinbellamy@gmail.com>
2023-01-15 08:12:54 -0500

    Max-min arrays

---
+++ maxMinArrays.js
@@ -0,0 +1,31 @@
+/* 
+7 kyu
+
+In this Kata, you will be given an array of unique elements, and your task is to rearrange the values so that the first max value is followed by the first minimum, followed by second max value then second min value, etc.
+
+For example:
+
+solve([15,11,10,7,12]) = [15,7,12,10,11]
+The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
+
+More examples in the test cases.
+
+Good luck!
+*/
+
+function solve(arr) {
+  let pointer1 = 0;
+  let pointer2 = arr.length - 1;
+
+  arr.sort((a, b) => b - a);
+  const finalArr = [];
+
+  while (pointer1 <= pointer2) {
+    finalArr.push(arr[pointer1]);
+    if (pointer1 === pointer2) break;
+    finalArr.push(arr[pointer2]);
+    pointer1++;
+    pointer2--;
+  }
+  return finalArr;
+}
