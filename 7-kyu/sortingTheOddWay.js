c4a083bbc6953d3d9ffa3d8fdce7bec44c3568ca
Febin Bellamy <febinbellamy@gmail.com>
2023-01-28 00:24:19 -0500

    Sorting the odd way

---
+++ 7-kyu/sortingTheOddWay.js
@@ -0,0 +1,17 @@
+/* 
+7 kyu
+
+Given an array of numbers, sort them in such a manner that all the odd numbers in the array are sorted in ascending order and the even numbers are sorted in descending order after the last odd number. For example [1,2,3,4,5,6,7,8,9] produces the output [1,3,5,7,9,8,6,4,2]. If the array contains decimals, round them down while checking for odd/even. The output must have the original numbers!
+
+If there are no issues with the kata and you successfully completed it, please rank and upvote!
+*/
+
+function sortItOut(array) {
+  const sortedOddNums = array
+    .filter((num) => Math.floor(num) % 2 !== 0)
+    .sort((a, b) => a - b);
+  const sortedEvenNums = array
+    .filter((num) => Math.floor(num) % 2 === 0)
+    .sort((a, b) => b - a);
+  return [...sortedOddNums, ...sortedEvenNums];
+}
