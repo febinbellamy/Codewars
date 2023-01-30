cd56fcb3e6c76dd687cf44ba0d481aeb6197ac0e
Febin Bellamy <febinbellamy@gmail.com>
2023-01-30 07:22:09 -0500

    Find Your Villain Name

---
+++ 7-kyu/findYourVillainName.js
@@ -0,0 +1,84 @@
+/* 
+7 kyu
+
+Create a function that returns a villain name based on the user's birthday. The birthday will be passed to the function as a valid Date object, so for simplicity, there's no need to worry about converting strings to dates.
+
+The first name will come from the month, and the last name will come from the last digit of the date:
+
+Month -> first name
+January -> "The Evil"
+February -> "The Vile"
+March -> "The Cruel"
+April -> "The Trashy"
+May -> "The Despicable"
+June -> "The Embarrassing"
+July -> "The Disreputable"
+August -> "The Atrocious"
+September -> "The Twirling"
+October -> "The Orange"
+November -> "The Terrifying"
+December -> "The Awkward"
+
+Last digit of date -> last name
+0 -> "Mustache"
+1 -> "Pickle"
+2 -> "Hood Ornament"
+3 -> "Raisin"
+4 -> "Recycling Bin"
+5 -> "Potato"
+6 -> "Tomato"
+7 -> "House Cat"
+8 -> "Teaspoon"
+9 -> "Laundry Basket"
+
+The returned value should be a string in the form of "First Name Last Name".
+
+For example, a birthday of November 18 would return "The Terrifying Teaspoon"
+*/
+
+function getVillainName(birthday) {
+  const m = [
+    "Evil",
+    "Vile",
+    "Cruel",
+    "Trashy",
+    "Despicable",
+    "Embarrassing",
+    "Disreputable",
+    "Atrocious",
+    "Twirling",
+    "Orange",
+    "Terrifying",
+    "Awkward",
+  ];
+  const d = [
+    "Mustache",
+    "Pickle",
+    "Hood Ornament",
+    "Raisin",
+    "Recycling Bin",
+    "Potato",
+    "Tomato",
+    "House Cat",
+    "Teaspoon",
+    "Laundry Basket",
+  ];
+  const bDayArr = birthday.toString().split(" ");
+  const months = {
+    Jan: 0,
+    Feb: 1,
+    Mar: 2,
+    Apr: 3,
+    May: 4,
+    Jun: 5,
+    Jul: 6,
+    Aug: 7,
+    Sep: 8,
+    Oct: 9,
+    Nov: 10,
+    Dec: 11,
+  };
+  const indexOfMonth = months[bDayArr[1]];
+  const lastDigitOfDate = bDayArr[2][1];
+  return `The ${m[indexOfMonth]} ${d[+lastDigitOfDate]}`;
+}
