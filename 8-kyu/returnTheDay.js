0e4ca5f69888c932d1d43b779582f38daf39c158
Febin Bellamy <febinbellamy@gmail.com>
2023-01-31 07:06:17 -0500

    Return the day

---
+++ 8-kyu/returnTheDay.js
@@ -0,0 +1,28 @@
+/* 
+8 kyu
+
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+*/
+
+function whatday(num) {
+  const weekdays = {
+    1: "Sunday",
+    2: "Monday",
+    3: "Tuesday",
+    4: "Wednesday",
+    5: "Thursday",
+    6: "Friday",
+    7: "Saturday",
+  };
+  if (!weekdays[num]) return "Wrong, please enter a number between 1 and 7";
+  return weekdays[num];
+}
