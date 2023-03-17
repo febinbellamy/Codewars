bbd8cd7a2ebc96af09a2a9d093f2a16de5eef84c
Febin Bellamy <febinbellamy@gmail.com>
2023-03-17 16:47:11 -0400

    Valid Parentheses

---
+++ 7-kyu/validParentheses.js
@@ -0,0 +1,39 @@
+/* 
+7 kyu
+
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+
+Constraints
+0 <= length of input <= 100
+
+- All inputs will be strings, consisting only of characters ( and ).
+- Empty strings are considered balanced (and therefore valid), and will be tested.
+- For languages with mutable strings, the inputs should not be mutated.
+
+Protip: If you are trying to figure out why a string of parentheses is invalid, paste the parentheses into the code editor, and let the code highlighting show you!
+*/
+
+function validParentheses(parenStr) {
+  // Your code here
+
+  const arr = [];
+
+  for (let i = 0; i < parenStr.length; i++) {
+    let currentChar = parenStr[i];
+    if (currentChar === ")" && arr[arr.length - 1] === "(") {
+      arr.pop();
+    } else if (currentChar === "(") {
+      arr.push(currentChar);
+    } else {
+      return false;
+    }
+  }
+
+  return arr.length > 0 ? false : true;
+}
