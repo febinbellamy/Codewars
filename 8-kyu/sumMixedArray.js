3789b3808ed8a4633b3c61a4ac41d90348d57fce
Febin Bellamy <febinbellamy@gmail.com>
2023-03-21 04:55:48 -0400

    Sum Mixed Array

---
+++ 8-kyu/sumMixedArray.js
@@ -0,0 +1,11 @@
+/* 
+8 kyu
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+*/
+
+function sumMix(x) {
+  return x.reduce((a, c) => +a + +c, 0);
+}
