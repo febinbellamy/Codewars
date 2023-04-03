be799534de79e524476429fa1d26124773ae252c
Febin Bellamy <febinbellamy@gmail.com>
2023-04-03 10:02:12 -0400

    Between Extremes

---
+++ 7-kyu/betweenExtremes.js
@@ -0,0 +1,19 @@
+/* 
+7 kyu
+
+Given an array of numbers, return the difference between the largest and smallest values.
+
+For example:
+
+[23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+[1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+*/
+
+function betweenExtremes(numbers) {
+  //write your code here
+  numbers.sort((a, b) => b - a);
+  return numbers[0] - numbers[numbers.length - 1];
+}
