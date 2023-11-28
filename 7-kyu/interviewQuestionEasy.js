b6c2e9f43d3a0094a4038366a37ef55ca7367000
Febin Bellamy <febinbellamy@gmail.com>
2023-11-28 06:56:51 -0500

    Interview Question (easy)

---
+++ 7-kyu/interviewQuestionEasy.js
@@ -0,0 +1,42 @@
+/* 
+7 kyu
+
+You receive the name of a city as a string, and you need to return a string that shows how many times each letter shows up in the string by using asterisks (*).
+
+For example:
+"Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
+
+As you can see, the letter c is shown only once, but with 2 asterisks.
+
+The return string should include only the letters (not the dashes, spaces, apostrophes, etc). There should be no spaces in the output, and the different letters are separated by a comma (,) as seen in the example above.
+
+Note that the return string must list the letters in order of their first appearance in the original string.
+
+More examples:
+"Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
+"Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"
+
+Have fun! ;)
+*/
+
+function getStrings(city) {
+  const cityAndAsterisks = {};
+  for (let i = 0; i < city.toLowerCase().length; i++) {
+    let currentChar = city.toLowerCase()[i];
+    if (/[a-zA-Z]/.test(currentChar)) {
+      if (cityAndAsterisks[currentChar]) {
+        cityAndAsterisks[currentChar] += "*";
+      } else {
+        cityAndAsterisks[currentChar] = "*";
+      }
+    }
+  }
+
+  let finalStr = "";
+
+  for (let key in cityAndAsterisks) {
+    finalStr += `${key}:${cityAndAsterisks[key]},`;
+  }
+
+  return finalStr.slice(0, finalStr.length - 1);
+}
