629d25587f7ccb1151379ecd5c4ec4a2bdfb903e
Febin Bellamy <febinbellamy@gmail.com>
2023-12-03 07:16:40 -0500

    Concatenated Sum

---
+++ 7-kyu/concatenatedSum.js
@@ -0,0 +1,39 @@
+/*
+7 kyu
+
+The number 198 has the property that 198 = 11 + 99 + 88, i.e., if each of its digits is concatenated twice and then summed, the result will be the original number. It turns out that 198 is the only number with this property. However, the property can be generalized so that each digit is concatenated n times and then summed.
+
+Examples
+original number = 2997 , n=3
+
+2997 = 222 + 999 + 999 + 777 and here each digit is concatenated three times.
+
+original number=-2997 , n=3
+
+-2997 = -222 - 999 - 999 - 777 and here each digit is concatenated three times.
+
+Task
+Write a function named checkConcatenatedSum that tests if a number has this generalized property.
+*/
+
+function checkConcatenatedSum(number, n) {
+  let str = String(number);
+  let totalSum = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let currentDigit = str[i];
+    if (currentDigit === "-") continue;
+
+    let strNum = "";
+    for (let j = 0; j < n; j++) {
+      strNum += currentDigit;
+    }
+
+    if (str[0] === "-") {
+      totalSum -= +strNum;
+    } else {
+      totalSum += +strNum;
+    }
+  }
+  return totalSum === number;
+}
