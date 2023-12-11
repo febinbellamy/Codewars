70072be7c727f4db846aa003864da9957e5ee383
Febin Bellamy <febinbellamy@gmail.com>
2023-12-11 08:29:34 -0500

    Working with arrays II

---
+++ 7-kyu/workingWithArraysII.js
@@ -0,0 +1,14 @@
+/* 
+7 kyu
+
+In this kata the function returns an array/list like the one passed to it but with its nth element removed (with 0 <= n <= array/list.length - 1). The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+Good luck!
+*/
+
+function removeNthElement(arr, n) {
+  // Fix it
+  var arrCopy = [...arr];
+  arrCopy.splice(n, 1); // removes the nth element
+  return arrCopy;
+}
