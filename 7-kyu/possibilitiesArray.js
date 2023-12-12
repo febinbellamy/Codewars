1477576a23620811d3b6400c8679415122a79ef6
Febin Bellamy <febinbellamy@gmail.com>
2023-12-12 05:21:05 -0500

    Possibilities Array

---
+++ 7-kyu/possibilitiesArray.js
@@ -0,0 +1,22 @@
+/* 
+7 kyu
+
+A non-empty array a of length n is called an array of all possibilities if it contains all numbers between [0,a.length-1].Write a method named isAllPossibilities that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+Example:
+a=[1,2,0,3]
+a.length-1=3 
+a includes [0,3] ,hence the function should return true
+*/
+
+function isAllPossibilities(x) {
+  if (x.length === 0) return false;
+
+  x.sort((a, b) => a - b);
+
+  for (let i = 0; i < x.length; i++) {
+    let currentNum = x[i];
+    if (i !== currentNum) return false;
+  }
+  return true;
+}
