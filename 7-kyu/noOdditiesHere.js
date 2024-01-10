224f234ed3cc401c228c616d5b74d28928aac585
Febin Bellamy <febinbellamy@gmail.com>
2024-01-10 10:10:46 -0500

    No oddities here

---
+++ 7-kyu/noOdditiesHere.js
@@ -0,0 +1,12 @@
+/* 
+7 kyu
+
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+*/
+
+function noOdds(values) {
+  // Return all non-odd values
+  return values.filter((num) => num % 2 === 0);
+}
