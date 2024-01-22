5264a3b8c6ebb3fbb37290ed29031549d380dd68
Febin Bellamy <febinbellamy@gmail.com>
2024-01-22 06:24:53 -0500

    Shifty Closures

---
+++ 8-kyu/shiftyClosures.js
@@ -0,0 +1,14 @@
+/* 
+8 kyu
+
+Functional closures can get overly attached. Set them straight!
+
+Why doesn't greetAbe() actually greet Abe?
+
+
+*/
+
+let name = "Abe";
+const greetAbe = () => "Hello, " + name + "!";
+let name2 = "Ben";
+const greetBen = () => "Hello, " + name2 + "!";
