bd8a75953dfb4bff5b7f1f4e92823e626cca4640
Febin Bellamy <febinbellamy@gmail.com>
2024-01-31 06:23:28 -0500

    L1: Bartender, drinks

---
+++ 8-kyu/lOneBartenderDrinks.js
@@ -0,0 +1,34 @@
+/* 
+8 kyu
+
+Complete the function that receives as input a string, and produces outputs according to the following table:
+
+Input	Output
+"Jabroni"	"Patron Tequila"
+"School Counselor"	"Anything with Alcohol"
+"Programmer"	"Hipster Craft Beer"
+"Bike Gang Member"	"Moonshine"
+"Politician"	"Your tax dollars"
+"Rapper"	"Cristal"
+anything else	"Beer"
+
+Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+*/
+
+function getDrinkByProfession(param) {
+  const table = {
+    jabroni: "Patron Tequila",
+    "school counselor": "Anything with Alcohol",
+    programmer: "Hipster Craft Beer",
+    "bike gang member": "Moonshine",
+    politician: "Your tax dollars",
+    rapper: "Cristal",
+  };
+  if (param.toLowerCase() in table) {
+    return table[param.toLowerCase()];
+  } else {
+    return "Beer";
+  }
+}
