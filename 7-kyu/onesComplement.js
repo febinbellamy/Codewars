663c60e28f20165046cabd3fcf8697e32fd9e8c2
Febin Bellamy <febinbellamy@gmail.com>
2024-02-26 08:24:16 -0500

    Ones' Complement

---
+++ 7-kyu/onesComplement.js
@@ -0,0 +1,23 @@
+/* 
+7 kyu
+
+The Ones' Complement of a binary number is the number obtained by swapping all the 0s for 1s and all the 1s for 0s.
+
+For any given binary number,formatted as a string, return the Ones' Complement of that number.
+
+Examples
+"0"    -> "1"
+"1"    -> "0"
+"000"  -> "111"
+"1001" -> "0110"
+"1001" -> "0110"
+*/
+
+function onesComplement(n) {
+  // your code here
+  const splitStr = n.split("");
+  for (let i = 0; i < splitStr.length; i++) {
+    splitStr[i] = splitStr[i] === "0" ? "1" : "0";
+  }
+  return splitStr.join("");
+}
