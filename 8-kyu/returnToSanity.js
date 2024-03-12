43bb8ca6c03649fccefd299189c9ca046393f428
Febin Bellamy <febinbellamy@gmail.com>
2024-03-12 08:36:53 -0400

    Return to Sanity

---
+++ 8-kyu/returnToSanity.js
@@ -0,0 +1,10 @@
+/* 
+8 kyu
+
+This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+
+function mystery() {
+  const results = { sanity: "Hello" };
+  return results;
+}
