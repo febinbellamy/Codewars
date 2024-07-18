b03ac2dbffb219780260d8d6ae5957eab7439f9d
Febin Bellamy <febinbellamy@gmail.com>
2024-07-18 10:48:15 -0400

    Divisible by previous digit?

---
+++ 7-kyu/divisibleByPreviousDigit.js
@@ -0,0 +1,26 @@
+/* 
+7 kyu
+
+Take a number and check each digit if it is divisible by the digit on its left checked and return an array of booleans.
+
+The booleans should always start with false becase there is no digit before the first one.
+
+Examples
+73312        => [false, false, true, false, true]
+2026         => [false, true, false, true]
+635          => [false, false, false]
+
+*** Remember 0 is evenly divisible by all integers but not the other way around ***
+*/
+
+function divisibleByLast(n) {
+  // TODO
+  const result = [false];
+  const strNum = String(n);
+  for (let i = 1; i < strNum.length; i++) {
+    let currentDigit = strNum[i];
+    let prevDigit = strNum[i - 1];
+    currentDigit % prevDigit === 0 ? result.push(true) : result.push(false);
+  }
+  return result;
+}
