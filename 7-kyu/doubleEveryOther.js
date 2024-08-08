fcf7fbf2f6af71fa66bb59742d6b32a467085821
Febin Bellamy <febinbellamy@gmail.com>
2024-08-08 10:29:36 -0400

    Double Every Other

---
+++ 7-kyu/doubleEveryOther.js
@@ -0,0 +1,16 @@
+/* 
+7 kyu
+
+Write a function that doubles every second integer in a list, starting from the left.
+
+Example:
+For input array/list :
+[1,2,3,4]
+
+the function should return :
+[1,4,3,8]
+*/
+
+function doubleEveryOther(a) {
+  return a.map((elem, idx) => (idx % 2 !== 0 ? elem * 2 : elem));
+}
