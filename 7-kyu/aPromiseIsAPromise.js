792eded7e600e92180d178b0bd38ca235fd56371
Febin Bellamy <febinbellamy@gmail.com>
2024-09-10 16:26:09 -0400

    A Promise is a Promise

---
+++ 7-kyu/aPromiseIsAPromise.js
@@ -0,0 +1,10 @@
+/* 
+7 kyu
+
+Create a resolved javascript Promise that will return 'Hello World!'.
+*/
+
+const promiseHelloWorld = () => {
+  // replace this nonsense with your Promise
+  return Promise.resolve("Hello World!");
+};
