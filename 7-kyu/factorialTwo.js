8377a0dab63cae86697a1bdb88d26834ec4f5a1a
Febin Bellamy <febinbellamy@gmail.com>
2024-09-14 20:43:23 -0400

    Factorial

---
+++ 7-kyu/factorialTwo.js
@@ -0,0 +1,14 @@
+/* 
+7 kyu
+
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+*/
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
