dd1e5aecbd47a97dcf1de7c53a8603d0f1e98d0a
Febin Bellamy <febinbellamy@gmail.com>
2024-11-07 08:28:45 -0500

    Regexp Basics - is it a digit?

---
+++ 8-kyu/regexpBasicsIsItADigit.js
@@ -0,0 +1,9 @@
+/* 
+8 kyu
+
+Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+*/
+
+String.prototype.digit = function () {
+  return /[0-9]/.test(this) && this.length === 1;
+};
