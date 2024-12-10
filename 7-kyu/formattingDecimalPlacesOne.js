2d92a2ce5b94ed018fb3f07b90b0d27494db43bc
Febin Bellamy <febinbellamy@gmail.com>
2024-12-10 10:28:41 -0500

    Formatting decimal places #1

---
+++ 7-kyu/formattingDecimalPlacesOne.js
@@ -0,0 +1,22 @@
+/* 
+7 kyu
+
+Each floating-point number should be formatted that only the first two decimal places are returned. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Don't round the numbers! Just cut them after two decimal places!
+
+Right examples:  
+32.8493 is 32.84  
+14.3286 is 14.32
+
+Incorrect examples (e.g. if you round the numbers):  
+32.8493 is 32.85  
+14.3286 is 14.33
+*/
+
+function twoDecimalPlaces(number) {
+  // Your code here
+  const strNum = String(number);
+  const indexOfDecimal = strNum.indexOf(".");
+  return +strNum.slice(0, indexOfDecimal + 3);
+}
