2dc32f401de04da6701dc96e676ba672c031c94e
Febin Bellamy <febinbellamy@gmail.com>
2025-02-26 09:43:38 -0500

    Add solution - CodeHub

---
+++ 7-kyu/extending-javascript-objects-simple-math-methods/extending-javascript-objects-simple-math-methods.js
@@ -0,0 +1,24 @@
+Number.prototype.add = function(n) {
+  return this + n;
+}
+​
+Number.prototype.sub = function(n) {
+  return this - n;
+}
+​
+Number.prototype.mul = function(n) {
+  return this * n;
+}
+​
+Number.prototype.div = function(n) {
+  return this / n;
+}
+​
+Number.prototype.pow = function(n) {
+  return this ** n;
+}
+​
+Number.prototype.sqr = function() {
+  return Math.sqrt(this);
+}
+​
