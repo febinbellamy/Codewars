691e3f351a9374e3f22877f02179ac83c481a1d2
Febin Bellamy <febinbellamy@gmail.com>
2025-03-14 08:54:33 -0400

    Add solution - CodeHub

---
+++ 7-kyu/a-no-more-bugs-trilogy-episode-3-make-a-player/a-no-more-bugs-trilogy-episode-3-make-a-player.js
@@ -0,0 +1,9 @@
+function Player(name, position, age, dribbling, pass, shoot) {
+  // Have fun!
+  this.name = name
+  this.position = position
+  this.age = age
+  this.dribbling = dribbling
+  this.pass = pass
+  this.shoot = shoot
+}
