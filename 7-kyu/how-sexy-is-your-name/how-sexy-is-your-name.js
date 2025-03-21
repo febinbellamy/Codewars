dd28ca06c22bdb3969be29b67231b8c330f76eec
Febin Bellamy <febinbellamy@gmail.com>
2025-03-21 09:29:01 -0400

    Add solution - CodeHub

---
+++ 7-kyu/how-sexy-is-your-name/how-sexy-is-your-name.js
@@ -0,0 +1,12 @@
+const sexyName = (name) => {
+  const finalScore = name.toUpperCase().split('').map((letter) => letter in SCORES ? SCORES[letter] : 0).reduce((a,c) => a+c, 0);
+  if (finalScore <= 60) {
+    return "NOT TOO SEXY"
+  } else if (finalScore >= 61 && finalScore <= 300) {
+    return "PRETTY SEXY"
+  } else if (finalScore >= 301 && finalScore <= 599) {
+    return "VERY SEXY"
+  } else {
+    return "THE ULTIMATE SEXIEST";
+  }
+}
