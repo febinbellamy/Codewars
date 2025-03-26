9805388cfdbaf802163b1f0b28a9eb6d8833342f
Febin Bellamy <febinbellamy@gmail.com>
2025-03-26 09:07:02 -0400

    Add solution - CodeHub

---
+++ 7-kyu/learning-typescript-classes-and-interfaces-inheritance/learning-typescript-classes-and-interfaces-inheritance.ts
@@ -0,0 +1,29 @@
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`
+  }
+}
+​
+export class Shark extends Animal {
+  constructor(name: string, age: number, status: string) {
+    super(name, age, 0, "shark", status);
+  }
+}
+​
+export class Cat extends Animal {
+  constructor(name: string, age: number, status: string) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce(): string {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
+  }
+}
+​
+export class Dog extends Animal {
+ master: string;
+  constructor(name: string, age: number, status: string, master: string) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  greetMaster(): string {
+    return `Hello ${this.master}`;
+  }
+}
