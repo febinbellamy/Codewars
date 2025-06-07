923bea19fa0f0d2863e27c1298b7be84c1fb3b95
Febin Bellamy <febinbellamy@gmail.com>
2025-06-07 08:01:26 -0400

    Add solution - CodeHub

---
+++ 8-kyu/broken-counter/broken-counter.cs
@@ -0,0 +1,31 @@
+public class Counter
+{
+    private int _value;
+​
+    public int Value
+    {
+        get
+        {
+            return _value;
+        }
+        private set
+        {
+            _value = value;
+        }
+    }
+    
+    public Counter()
+    {
+        Value = 0;
+    }
+    
+    public void Increase()
+    {
+        Value++;
+    }
+    
+    public void Reset()
+    {
+        Value = 0;
+    }
+}
