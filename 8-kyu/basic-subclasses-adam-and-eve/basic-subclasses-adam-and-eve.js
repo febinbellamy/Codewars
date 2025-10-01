eef3f11aceab10402ab079a8c4bbf5a985879dbb
Febin Bellamy <febinbellamy@gmail.com>
2025-10-01 09:44:48 -0400

    Add solution - CodeHub

---
+++ 8-kyu/basic-subclasses-adam-and-eve/basic-subclasses-adam-and-eve.js
@@ -0,0 +1,12 @@
+class God{
+/**
+ * @returns Human[]
+ */
+  static create(){
+    return [new Man(), new Woman()];
+  }
+}
+​
+class Human {}
+class Man extends Human {}
+class Woman extends Human {}
