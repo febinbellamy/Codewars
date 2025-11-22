b5d7c80b2599abfc380ece2ea0f36038a4249794
Febin Bellamy <febinbellamy@gmail.com>
2025-11-22 10:24:38 -0500

    Add solution - CodeHub

---
+++ 7-kyu/simple-fun-number-50-array-conversion/simple-fun-number-50-array-conversion.js
@@ -0,0 +1,18 @@
+function arrayConversion(arr) {
+  //coding and coding..
+  // replace each pair with their sum
+  let currentArr = arr.slice(0);
+  while(currentArr.length !== 1) {
+    const sums = [];
+    for(let i = 0; i < currentArr.length; i+=2) {
+      sums.push(currentArr[i] + currentArr[i+1]);
+    }
+    if (sums.length === 1) return sums[0]; 
+    const products = [];
+    for(let j = 0; j < sums.length; j += 2) {
+      products.push(sums[j + 1] * sums[j])
+    }
+    currentArr = products.slice(0);
+  }
+  return currentArr[0];  
+}
